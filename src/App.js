bd2c391515222dc1b3be1aca71c057040f6525a2
mvuas10 <mvuas10@gmail.com>
2020-05-06 11:51:55 +0200

    Home route added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,15 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/docSchedule">Doctor Schedule</Link>
-      <Link to="/patientSignup">Patient Signup</Link>
-      <Link to="/patientDatabase">Patient Database</Link>
+      <nav>
+        <br />
+        <Link to="/">Home</Link> {" | "}
+        <Link to="/docSchedule">Doctor Schedule</Link> {" | "}
+        <Link to="/patientSignup">Patient Signup</Link> {" | "}
+        <Link to="/patientDatabase">Patient Database</Link>
+      </nav>
+      <br />
+      <hr />
     </div>
   );
 }
